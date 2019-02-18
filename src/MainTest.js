b1f8445582e13f90d4039fd78546658cfb135919
Sidney Ofosu-Ottopah <sofosu-ottopah@MacBook-Pro-40.local>
2019-02-18 09:28:30 +0000

    Added Storybook implementations for DOM plain testing

--- src/MainTest.js
+++ src/MainTest.js
@@ -9,11 +9,10 @@ import {BrowserRouter, Route } from 'react-router-dom';
 import SignIn from './components/other/SignIn.js';
 import SignUp from './components/other/SignUp';
 
+class MainTest extends Component{
+    render(){
 
-
-function render(){
-    ReactDOM.render(
-        
+      return(
         <Provider store={store}>
             <BrowserRouter >
                 <React.Fragment>
@@ -23,17 +22,10 @@ function render(){
                 </React.Fragment>
                     
             </BrowserRouter>
-        </Provider>
-        , 
-        document.getElementById('root'));
-    
-};
+        </Provider>    
+        
+      );
+    }
+}
 
-render()
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-
-
+export default MainTest
